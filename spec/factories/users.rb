093bda976479ebc25779a65ccb554d84b2d96ef1
Ay00Mo <ayaka.hatano@gmail.com>
2024-08-28 22:02:57 +0900

    FactoryBotのpasswordを必ず半角英数字混合になるように修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.name }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { "#{Faker::Internet.password(min_length: 6)}1a" }
     password_confirmation { password }
     last_name { ['山田', '佐藤', '田中'].sample } # rubocop:disable Style/WordArray
     first_name { ['太郎', '次郎', '花子'].sample } # rubocop:disable Style/WordArray
