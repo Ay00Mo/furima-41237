dec59f9d54ca9b5fcbba2f0c392cce5c8a4758e4
Ay00Mo <ayaka.hatano@gmail.com>
2024-08-31 18:40:29 +0900

    情報をテーブルに保存する実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id,
+                                 :price).merge(user_id: current_user.id)
+  end
 end
