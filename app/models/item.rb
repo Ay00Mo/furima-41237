68682f5a496f2d673ecef9d48f6e0862d2068f25
Ay00Mo <ayaka.hatano@gmail.com>
2024-08-31 17:04:10 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :days_to_ship
+
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+
+  validates :category_id, :condition_id, :shipping_fee_id,
+            :prefecture_id, :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
 end
