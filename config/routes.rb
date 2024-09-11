9ed2113f153a8a9f486fac2b631c3146e48cbd12
Ay00Mo <ayaka.hatano@gmail.com>
2024-09-11 20:18:35 +0900

    showアクションのルーディングとコントローラーの記述

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "items#index"
-  resources :items, only: [:new, :create] do
+  resources :items, only: [:index, :new, :create, :show] do
   end
 end
