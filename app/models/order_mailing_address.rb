4a6a57236076e55493e98aecf264e437d267fe4e
Ay00Mo <ayaka.hatano@gmail.com>
2024-09-24 08:31:26 +0900

    バリデーション

---
+++ app/models/order_mailing_address.rb
@@ -0,0 +1,14 @@
+class OrderMainlingAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number, format: {with /\A[0-9]\z/, message: "is invalid. Only allows numbers"}
+  end
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+end
