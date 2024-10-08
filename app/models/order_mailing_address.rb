b3167f429657d13afc33b5d658a5ba3cad1e26af
Ay00Mo <ayaka.hatano@gmail.com>
2024-10-08 20:03:26 +0900

    トークンに対するバリデーションを設定

--- app/models/order_mailing_address.rb
+++ app/models/order_mailing_address.rb
@@ -1,6 +1,6 @@
 class OrderMailingAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+  attr_accessor :token, :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
 
   with_options presence: true do
     validates :user_id
@@ -10,9 +10,10 @@ class OrderMailingAddress
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Only allows numbers' }
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :token, presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id) # rubocop:disable Style/HashSyntax
-    MailingAddress.create(postal_code: postal_code, city: city, address: address, phone_number: phone_number, order_id: order.id) # rubocop:disable Style/HashSyntax
+    MailingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number, order_id: order.id) # rubocop:disable Style/HashSyntax,Layout/LineLength
   end
 end
